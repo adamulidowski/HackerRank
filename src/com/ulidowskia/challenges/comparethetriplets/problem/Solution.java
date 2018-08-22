035de39d62db2dc1e2caa6384dadedc4235f2be9
Adam Ulidowski <adam.ulidowski@wp.pl>
2018-08-22 20:31:44 +0200

    Compare the Triplets

---
+++ src/com/ulidowskia/challenges/comparethetriplets/problem/Solution.java
@@ -0,0 +1,69 @@
+package com.ulidowskia.challenges.comparethetriplets.problem;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+/**
+ * Created by Adam on 22.08.2018.
+ */
+public class Solution {
+
+    // Complete the solve function below.
+    static int[] solve(int[] a, int[] b) {
+        int [] scores= {0, 0};
+        for(int i=0; i<a.length; i++){
+            if(a[i] > b[i]){
+                scores[0]++;
+            } else if(a[i] < b[i]){
+                scores[1]++;
+            }
+        }
+        return scores;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int[] a = new int[3];
+
+        String[] aItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < 3; i++) {
+            int aItem = Integer.parseInt(aItems[i]);
+            a[i] = aItem;
+        }
+
+        int[] b = new int[3];
+
+        String[] bItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < 3; i++) {
+            int bItem = Integer.parseInt(bItems[i]);
+            b[i] = bItem;
+        }
+
+        int[] result = solve(a, b);
+
+        for (int i = 0; i < result.length; i++) {
+            bufferedWriter.write(String.valueOf(result[i]));
+
+            if (i != result.length - 1) {
+                bufferedWriter.write(" ");
+            }
+        }
+
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
