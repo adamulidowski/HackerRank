920f36cfa4f4bd701c32e089379f4a5198858c40
Adam Ulidowski <adam.ulidowski@wp.pl>
2018-08-25 20:33:17 +0200

    PLus Minus

---
+++ src/com/ulidowskia/challenges/plusminus/problem/Solution.java
@@ -0,0 +1,54 @@
+package com.ulidowskia.challenges.plusminus.problem;
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+
+public class Solution {
+
+    // Complete the plusMinus function below.
+    static void plusMinus(int[] arr) {
+        double length = arr.length;
+        double numOfPositive = 0;
+        double numOfNegative = 0;
+        double numOfZeros = 0;
+        for(int i =0; i<arr.length; i++){
+            if(arr[i]<0){
+                numOfNegative ++;
+            } else if(arr[i]>0){
+                numOfPositive++;
+            }else{
+                numOfZeros++;
+            }
+        }
+        DecimalFormat df = new DecimalFormat("#.#####");
+        System.out.println(df.format(numOfPositive/length));
+        System.out.println(df.format(numOfNegative/length));
+        System.out.println(df.format(numOfZeros/length));
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[] arr = new int[n];
+
+        String[] arrItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int arrItem = Integer.parseInt(arrItems[i]);
+            arr[i] = arrItem;
+        }
+
+        plusMinus(arr);
+
+        scanner.close();
+    }
+}
