acd024c5532b5515923057772ce32f7466f65156
Adam Ulidowski <adam.ulidowski@wp.pl>
2019-03-13 15:05:32 +0100

    Test

--- src/com/ulidowskia/challenges/libraryfine/problem/Solution.java
+++ src/com/ulidowskia/challenges/libraryfine/problem/Solution.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
 
 public class Solution {
 
-    // Complete the libraryFine function below.
+
     static int libraryFine(int d1, int m1, int y1, int d2, int m2, int y2) {
         if (y1 < y2) return 0;
         if (y1 > y2) return (10000 * (y1 - y2));
